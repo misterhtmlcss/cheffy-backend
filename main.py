3099b90e01fbe74eb57b42635eff3eebfb67920b
Matt Fisher <matt.fisherrr@gmail.com>
2019-05-10 14:46:21 -0600

    Cleaning stuff up

--- main.py
+++ main.py
@@ -28,15 +28,16 @@ cur = conn.cursor()
 app = Flask(__name__)
 
 # Serves the chef profile API
-@app.route('/api/chef/<chef_name>')
-def chef_api(chef_name):
+@app.route('/api/chef', methods=['GET'])
+def chef_api():
+    chef_name = request.get_json()['chef']
     cur.execute(f"SELECT * FROM Chef WHERE name = '{chef_name}'")
     currChef = cur.fetchone()
     return jsonify({'name': currChef[1], 'phone': currChef[4], 'specialties': [ft for ft in currChef[3].split(', ')], 'description': currChef[5]})
 
 
 # Serves the home page API
-@app.route('/api/homepage')
+@app.route('/api/homepage', methods=['GET'])
 def home_api():
 
     # Top box
@@ -45,28 +46,28 @@ def home_api():
     count = int(cur.fetchall()[0][0])
     cur.execute(
         f"SELECT food_type,picture FROM food_type OFFSET {random.randint(1,count)} LIMIT 5")
-    for result in cur.fetchall():
-        topScroller.append({'foodName': result[0], 'picture': result[1]})
+    for food_name, picture in cur.fetchall():
+        topScroller.append({'foodName': food_name, 'picture': picture})
 
     # Food Types
     food_types = []
     cur.execute(f"SELECT food_type,picture FROM food_type")
-    for result in cur.fetchall():
-        food_types.append({'foodName': result[0], 'picture': result[1]})
+    for food_name, picture in cur.fetchall():
+        food_types.append({'foodName': food_name, 'picture': picture})
 
     # Chefs
     chefs = []
     cur.execute(f"SELECT name, picture FROM chef")
     chef_list = random.sample(cur.fetchall(), 6)
-    for result in chef_list:
-        chefs.append({'name': result[0], 'picture': result[1]})
+    for name, picture in chef_list:
+        chefs.append({'name': name, 'picture': picture})
 
     return jsonify({'topScroller': topScroller, 'foodTypes': food_types, 'chefs': chefs})
 
 # Serves the chef list API, after a food type is selected
-@app.route('/api/<food_type>')
-def chef_type_list(food_type):
-    # SELECT chef_name FROM Chefs WHERE specialty LIKE '%{type}%'
+@app.route('/api/foodtype', methods=['GET'])
+def chef_type_list():
+    food_type = request.get_json()['foodtype']
     chefs = []
     cur.execute(f"SELECT name, picture FROM chef WHERE specialty LIKE '%{food_type.lower()}%'")
     for chef in cur.fetchall():
@@ -74,9 +75,18 @@ def chef_type_list(food_type):
     return jsonify(chefs)
 
 # Some sort of search bar API
-@app.route('/api/search/<search_term>')
-def search_field(search_term):
-    return 'search results'
+@app.route('/api/search/', methods=['GET'])
+def search_field():
+    search_term = request.get_json()['searchterm']
+
+    # Search food types
+    cur.execute(f"SELECT food_type, picture FROM food_type WHERE food_type LIKE '%{search_term.lower()}%'")
+    food_type = [{"name":name,"picture":picture} for name, picture in cur.fetchall()]
+
+    # Search chefs
+    cur.execute(f"SELECT name, picture FROM chef WHERE name LIKE '%{search_term.lower()}%'")
+    chefs = [{"name":name,"picture":picture} for name, picture in cur.fetchall()]
+    return jsonify({"foodTypes":food_type,"chefs":chefs})
 
 
 if __name__ == '__main__':
